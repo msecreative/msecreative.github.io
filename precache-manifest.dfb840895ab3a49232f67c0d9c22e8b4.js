5d244fed1c4ce30ba7d7543743b50cc356162800
msecreative <47583511+msecreative@users.noreply.github.com>
2019-10-11 14:52:02 +0600

    My React App

---
+++ precache-manifest.dfb840895ab3a49232f67c0d9c22e8b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1639e799743c6b1121143b44641cf1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0792fce70edc174fe95f",
+    "url": "/static/css/main.bbc4d71b.chunk.css"
+  },
+  {
+    "revision": "ceac375e5b4e2bce520c",
+    "url": "/static/js/2.41a401d5.chunk.js"
+  },
+  {
+    "revision": "0792fce70edc174fe95f",
+    "url": "/static/js/main.1e656ad7.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
